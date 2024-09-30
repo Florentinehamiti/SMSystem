60f59b3cfa5346f7739a94927b751e8631ce0206
Florentinehamiti <85936110+Florentinehamiti@users.noreply.github.com>
2024-09-30 16:25:55 +0200

    first commit

---
+++ Presentation/Areas/Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Presentation.Areas.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; } = null!;
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
