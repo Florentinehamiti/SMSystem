d60768593d96ee4241631580e1fb247294769630
Florentine Hamiti <florentine.hamiti@riinvest.net>
2025-01-27 22:34:39 +0100

    Educator area

---
+++ SMSystem.Data/Migrations/20250127213325_newTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class newTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
